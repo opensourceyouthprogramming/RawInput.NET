c20a622a2de159097fb8af4e52835b9f783834b8
shadowlif <2185647+DogeAmazed@users.noreply.github.com>
2017-07-31 15:49:59 +0200

    Added example application

---
+++ RawInputDotNet/ExampleApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExampleApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExampleApplication")]
+[assembly: AssemblyCopyright("Copyright © DogeAmazed 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29851e61-ef72-4097-abe4-f790bf58de3a")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
