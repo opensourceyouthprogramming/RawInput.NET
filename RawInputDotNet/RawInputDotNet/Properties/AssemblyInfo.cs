149e02bbd6cc5788e7b6821d1583750daac4d42e
shadowlif <2185647+DogeAmazed@users.noreply.github.com>
2017-07-31 15:32:27 +0200

    Added VS project

---
+++ RawInputDotNet/RawInputDotNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RawInputDotNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RawInputDotNet")]
+[assembly: AssemblyCopyright("Copyright © DogeAmazed 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8e77cae-7538-4405-9ac9-ed71e666122c")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
